3904f7d1fb0513e850db097838c9d79b68821243
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2024-08-07 00:12:09 +0530

    [INJIMOB-1629] write different test cases for decoder class
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

---
+++ kotlin/openId4VP/src/test/java/io/mosip/openID4VP/shared/DecoderTest.kt
@@ -0,0 +1,31 @@
+package io.mosip.openID4VP.shared
+
+import io.mosip.openID4VP.authorizationRequest.exception.AuthorizationRequestExceptions
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThrows
+import org.junit.Test
+
+class DecoderTest {
+
+    @Test
+    fun `should throw InvalidInputException for empty input`() {
+        val encodedData = ""
+        val expectedValue = "Invalid Input: encoded data value cannot be empty or null"
+
+        val invalidInputException = assertThrows(AuthorizationRequestExceptions.InvalidInput::class.java) {
+            Decoder.decodeBase64ToString(encodedData)
+        }
+
+        assertEquals(expectedValue,invalidInputException.message)
+    }
+
+    @Test
+    fun `should decode valid Base64 string`() {
+        val encodedData = "SGVsbG8gV29ybGQ="
+        val expectedDecodedString = "Hello World"
+
+        val decodedString = Decoder.decodeBase64ToString(encodedData)
+
+        assertEquals(expectedDecodedString, decodedString)
+    }
+}
