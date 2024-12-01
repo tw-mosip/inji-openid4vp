604f2499c1df73ad659db6d1c913b6929660ffcc
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2024-12-01 14:16:09 +0530

    [INJIMOB-2265] for fields of type string don't return false in validateField method if it's value is of type string and value is null as this is a valid scenario
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- kotlin/openID4VP/src/main/java/io/mosip/openID4VP/common/fieldValidator.kt
+++ kotlin/openID4VP/src/main/java/io/mosip/openID4VP/common/fieldValidator.kt
@@ -1,17 +1,14 @@
 import kotlinx.serialization.json.JsonNull
-import kotlinx.serialization.json.JsonPrimitive
 
 fun validateField(field: Any?, fieldType: String?): Boolean {
 	var res = true
 	when {
-		fieldType == "String" -> res =
-			field != "null" && field.toString().isNotEmpty() && field != JsonNull
+		fieldType == "String" -> res = field.toString().isNotEmpty() && field != JsonNull
 
 		fieldType?.startsWith("List") == true -> res =
 			field != JsonNull && (field as? List<*>)?.isNotEmpty() ?: false
 
 		fieldType == "Boolean" -> res = field != JsonNull && (field == true || field == false)
-		else -> res
 	}
 	return res
 }
