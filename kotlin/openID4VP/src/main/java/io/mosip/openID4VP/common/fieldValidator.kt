1772d57fd2e5546bc06bcb0b0889ef8e865fb43d
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2024-12-01 16:27:30 +0530

    [INJIMOB-2265] remove jsonNull check in fieldValidator method as we are already checking for it in deserializer method
    Signed-off-by: PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>

--- kotlin/openID4VP/src/main/java/io/mosip/openID4VP/common/fieldValidator.kt
+++ kotlin/openID4VP/src/main/java/io/mosip/openID4VP/common/fieldValidator.kt
@@ -1,14 +1,11 @@
-import kotlinx.serialization.json.JsonNull
-
 fun validateField(field: Any?, fieldType: String?): Boolean {
 	var res = true
 	when {
-		fieldType == "String" -> res = field.toString().isNotEmpty() && field != JsonNull
+		fieldType == "String" -> res = field.toString().isNotEmpty()
 
-		fieldType?.startsWith("List") == true -> res =
-			field != JsonNull && (field as? List<*>)?.isNotEmpty() ?: false
+		fieldType?.startsWith("List") == true -> res = (field as? List<*>)?.isNotEmpty() ?: false
 
-		fieldType == "Boolean" -> res = field != JsonNull && (field == true || field == false)
+		fieldType == "Boolean" -> res = (field == true || field == false)
 	}
 	return res
 }
