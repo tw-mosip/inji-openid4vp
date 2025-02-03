6595f21ca01dc0bbec1f57885b817e8aa8f7582a
Alka Prasad <Alka1703@users.noreply.github.com>
2025-02-03 18:37:03 +0530

    [INJIMOB-2761]: add tests and remove reduntant file (#34)
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>

--- kotlin/openID4VP/src/main/java/io/mosip/openID4VP/authorizationResponse/exception/AuthorizationResponseExceptions.kt
+++
@@ -1,5 +0,0 @@
-package io.mosip.openID4VP.authorizationResponse.exception
-
-sealed class AuthorizationResponseExceptions {
-	class JsonEncodingException(fieldName: String): Exception("Error occurred while serializing the data - $fieldName")
-}
