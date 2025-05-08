d408abf42684b4143cbb076eb4ece1cadf663819
Alka Prasad <prasadalka1998@gmail.com>
2025-05-08 17:26:11 +0530

    [INJIMOB-3153]: update logic of populating the context of VP token
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>

--- kotlin/openID4VP/src/main/java/io/mosip/openID4VP/authorizationResponse/unsignedVPToken/types/ldp/UnsignedLdpVPTokenBuilder.kt
+++ kotlin/openID4VP/src/main/java/io/mosip/openID4VP/authorizationResponse/unsignedVPToken/types/ldp/UnsignedLdpVPTokenBuilder.kt
@@ -2,6 +2,10 @@ package io.mosip.openID4VP.authorizationResponse.unsignedVPToken.types.ldp
 
 import io.mosip.openID4VP.authorizationResponse.unsignedVPToken.UnsignedVPToken
 import io.mosip.openID4VP.authorizationResponse.unsignedVPToken.types.UnsignedVPTokenBuilder
+import io.mosip.openID4VP.common.Logger
+import io.mosip.openID4VP.common.convertJsonToMap
+
+private val className = UnsignedLdpVPToken::class.simpleName!!
 
 class UnsignedLdpVPTokenBuilder(
     private val verifiableCredential: List<String>,
@@ -10,8 +14,23 @@ class UnsignedLdpVPTokenBuilder(
 ): UnsignedVPTokenBuilder
 {
     override fun build(): UnsignedVPToken {
+        if(verifiableCredential.isEmpty()){
+            throw Logger.handleException(
+                exceptionType = "InvalidData",
+                message = "Ldp Verifiable Credential List is empty",
+                className = className
+            )
+        }
+
+        val context = verifiableCredential.map { vc ->
+            val verifiableCredentialMap = convertJsonToMap(vc)
+            val credential = (verifiableCredentialMap["verifiableCredential"] as Map<String, Any>)["credential"] as Map<String, Any>
+            ((credential["@context"] as List<*>)[0]).toString()
+        }.toSet()
+
+        println(context)
         return UnsignedLdpVPToken(
-            context = listOf("https://www.w3.org/2018/credentials/v1"),
+            context = context.toList(),
             type = listOf("VerifiablePresentation"),
             verifiableCredential = verifiableCredential,
             id = id,
