6053534208a75ef253ce8298dcca8380dadff17e
Alka Prasad <prasadalka1998@gmail.com>
2025-06-19 15:27:22 +0530

    [INJIMOB-3303]: add and fix test cases (#85)
    * [INJIMOB-3303]: add and fix test cases
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    * [INJIMOB-3303]: remove duplicate and empty classes
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>
    
    ---------
    
    Signed-off-by: Alka Prasad <prasadalka1998@gmail.com>

--- kotlin/openID4VP/src/test/java/io/mosip/openID4VP/OpenID4VPTest.kt
+++ kotlin/openID4VP/src/test/java/io/mosip/openID4VP/OpenID4VPTest.kt
@@ -1,46 +1,51 @@
-package io.mosip.openID4VP
+//package io.mosip.openID4VP
 //
 //import android.util.Log
+//import info.weboftrust.ldsignatures.canonicalizer.URDNA2015Canonicalizer
 //import io.mockk.clearAllMocks
 //import io.mockk.every
+//import io.mockk.mockkClass
 //import io.mockk.mockkConstructor
 //import io.mockk.mockkObject
 //import io.mockk.mockkStatic
+//import io.mockk.unmockkStatic
 //import io.mockk.verify
 //import io.mosip.openID4VP.common.UUIDGenerator
 //import io.mosip.openID4VP.constants.FormatType
 //import io.mosip.openID4VP.exceptions.Exceptions
 //import io.mosip.openID4VP.constants.HttpMethod
-//import io.mosip.openID4VP.authorizationResponse.vpTokenSigningResult.types.ldp.LdpVPTokenSigningResult
 //import io.mosip.openID4VP.authorizationResponse.unsignedVPToken.types.ldp.UnsignedLdpVPTokenBuilder
 //import io.mosip.openID4VP.authorizationResponse.unsignedVPToken.types.mdoc.UnsignedMdocVPTokenBuilder
+//import io.mosip.openID4VP.common.URDNA2015Canonicalization
 //import io.mosip.openID4VP.networkManager.NetworkManagerClient
 //import io.mosip.openID4VP.networkManager.exception.NetworkManagerClientExceptions.NetworkRequestFailed
 //import io.mosip.openID4VP.networkManager.exception.NetworkManagerClientExceptions.NetworkRequestTimeout
 //import io.mosip.openID4VP.testData.authorizationRequest
 //import io.mosip.openID4VP.testData.holderId
-//import io.mosip.openID4VP.testData.publicKey
 //import io.mosip.openID4VP.testData.setField
 //import io.mosip.openID4VP.testData.unsignedVPTokens
 //import io.mosip.openID4VP.testData.ldpvpTokenSigningResults
 //import io.mosip.openID4VP.testData.ldpCredential1
 //import io.mosip.openID4VP.testData.ldpCredential2
+//import io.mosip.openID4VP.testData.ldpVPTokenSigningResult
 //import io.mosip.openID4VP.testData.mdocCredential
+//import io.mosip.openID4VP.testData.signatureSuite
 //import io.mosip.openID4VP.testData.unsignedLdpVPToken
 //import io.mosip.openID4VP.testData.unsignedMdocVPToken
+//import io.mosip.openID4VP.testData.vpTokenSigningPayload
 //import okhttp3.Headers
 //import okhttp3.mockwebserver.MockWebServer
 //import org.junit.After
 //import org.junit.Assert.assertEquals
 //import org.junit.Assert.assertThrows
 //import org.junit.Before
+//import org.junit.Ignore
 //import org.junit.Test
+//import java.util.UUID
 //
 //class OpenID4VPTest {
 //
 //    private lateinit var openID4VP: OpenID4VP
-//
-//
 //    private val selectedLdpCredentialsList = mapOf(
 //        "456" to mapOf(
 //            FormatType.LDP_VC to listOf(
@@ -53,14 +58,11 @@ package io.mosip.openID4VP
 //            )
 //        )
 //    )
-//
-//
 //    private val selectedMdocCredentialsList = mapOf(
 //        "123" to mapOf(
 //            FormatType.MSO_MDOC to listOf(mdocCredential)
 //        )
 //    )
-//
 //    private lateinit var mockWebServer: MockWebServer
 //    private lateinit var actualException: Exception
 //    private lateinit var expectedExceptionMessage: String
@@ -86,7 +88,7 @@ package io.mosip.openID4VP
 //            0
 //        }
 //
-//        openID4VP.constructUnsignedVPToken(selectedLdpCredentialsList, holderId )
+//        openID4VP.constructUnsignedVPToken(selectedLdpCredentialsList, holderId, signatureSuite )
 //    }
 //
 //    @After
@@ -95,40 +97,22 @@ package io.mosip.openID4VP
 //        mockWebServer.shutdown()
 //    }
 //
-//    //Test name and body does not match
 //    @Test
 //    fun `should construct VPToken using received selected verifiable credentials`() {
 //        mockkObject(UUIDGenerator)
-//        every { UUIDGenerator.generateUUID() } returns "649d581c-f291-4969-9cd5-2c27385a348f"
+//        every { UUIDGenerator.generateUUID() } returns "test-uuid-123"
 //
 //        mockkConstructor(UnsignedLdpVPTokenBuilder::class)
-//        every { anyConstructed<UnsignedLdpVPTokenBuilder>().build() } returns mapOf("UnsignedLdpVPToken" to unsignedLdpVPToken, "dataToSign" to "dataToSign")
+//        every { anyConstructed<UnsignedLdpVPTokenBuilder>().build() } returns mapOf("unsignedVPToken" to unsignedLdpVPToken, "vpTokenSigningPayload" to vpTokenSigningPayload)
 //
 //        mockkConstructor(UnsignedMdocVPTokenBuilder::class)
-//        every { anyConstructed<UnsignedMdocVPTokenBuilder>().build() } returns mapOf("UnsignedLdpVPToken" to unsignedMdocVPToken, "dataToSign" to "dataToSign")
+//        every { anyConstructed<UnsignedMdocVPTokenBuilder>().build() } returns mapOf("unsignedVPToken" to unsignedMdocVPToken, "vpTokenSigningPayload" to listOf(mdocCredential))
 //
-//
-//        val actualUnsignedVPTokens = openID4VP.constructUnsignedVPToken(selectedLdpCredentialsList + selectedMdocCredentialsList, holderId = holderId)
+//        val actualUnsignedVPTokens = openID4VP.constructUnsignedVPToken(selectedLdpCredentialsList + selectedMdocCredentialsList, holderId, signatureSuite)
 //
 //        val expectedUnsignedVPTokens = unsignedVPTokens
-//        assertEquals(expectedUnsignedVPTokens[FormatType.LDP_VC], actualUnsignedVPTokens[FormatType.LDP_VC])
-//        assertEquals(expectedUnsignedVPTokens[FormatType.MSO_MDOC], actualUnsignedVPTokens[FormatType.MSO_MDOC])
-//    }
-//
-//    @Test
-//    fun `should throw invalid input exception if any input param of VPTokenSigningResult class is empty`() {
-//        val ldpVPTokenSigningResult = LdpVPTokenSigningResult(
-//            "eyJiweyrtwegrfwwaBKCGSwxjpa5suaMtgnQ", "RsaSignature2018", publicKey, ""
-//        )
-//        val vpTokenSigningResults = mapOf(FormatType.LDP_VC to ldpVPTokenSigningResult)
-//        expectedExceptionMessage =
-//            "Invalid Input: ldp_vp_token_signing_result->domain value cannot be an empty string, null, or an integer"
-//        actualException =
-//            assertThrows(Exceptions.InvalidInput::class.java) {
-//                openID4VP.shareVerifiablePresentation(vpTokenSigningResults)
-//            }
-//
-//        assertEquals(expectedExceptionMessage, actualException.message)
+//        assertEquals(expectedUnsignedVPTokens[FormatType.LDP_VC]!!["unsignedVPToken"], actualUnsignedVPTokens[FormatType.LDP_VC])
+//        assertEquals(expectedUnsignedVPTokens[FormatType.MSO_MDOC]!!["unsignedVPToken"], actualUnsignedVPTokens[FormatType.MSO_MDOC])
 //    }
 //
 //    @Test
@@ -213,3 +197,292 @@ package io.mosip.openID4VP
 //        }
 //    }
 //}
+//
+
+
+package io.mosip.openID4VP
+
+import android.util.Log
+import io.mockk.*
+import io.mosip.openID4VP.authorizationRequest.AuthorizationRequest
+import io.mosip.openID4VP.authorizationRequest.Verifier
+import io.mosip.openID4VP.authorizationRequest.WalletMetadata
+import io.mosip.openID4VP.authorizationResponse.AuthorizationResponseHandler
+import io.mosip.openID4VP.authorizationResponse.unsignedVPToken.types.ldp.UnsignedLdpVPTokenBuilder
+import io.mosip.openID4VP.authorizationResponse.unsignedVPToken.types.mdoc.UnsignedMdocVPTokenBuilder
+import io.mosip.openID4VP.authorizationResponse.vpTokenSigningResult.types.ldp.VPResponseMetadata
+import io.mosip.openID4VP.common.Logger
+import io.mosip.openID4VP.common.URDNA2015Canonicalization
+import io.mosip.openID4VP.common.UUIDGenerator
+import io.mosip.openID4VP.constants.FormatType
+import io.mosip.openID4VP.constants.HttpMethod
+import io.mosip.openID4VP.exceptions.Exceptions
+import io.mosip.openID4VP.networkManager.NetworkManagerClient
+import io.mosip.openID4VP.networkManager.exception.NetworkManagerClientExceptions.NetworkRequestFailed
+import io.mosip.openID4VP.networkManager.exception.NetworkManagerClientExceptions.NetworkRequestTimeout
+import io.mosip.openID4VP.testData.*
+import okhttp3.Headers
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import foundation.identity.jsonld.JsonLDObject
+
+class OpenID4VPTest {
+
+    private lateinit var openID4VP: OpenID4VP
+    private val selectedLdpCredentialsList = mapOf(
+        "456" to mapOf(
+            FormatType.LDP_VC to listOf(
+                ldpCredential1,
+                ldpCredential2
+            )
+        ), "789" to mapOf(
+            FormatType.LDP_VC to listOf(
+                ldpCredential2
+            )
+        )
+    )
+    private val selectedMdocCredentialsList = mapOf(
+        "123" to mapOf(
+            FormatType.MSO_MDOC to listOf(mdocCredential)
+        )
+    )
+
+    @Before
+    fun setUp() {
+        mockkObject(NetworkManagerClient)
+        mockkStatic(Log::class)
+        every { Log.e(any(), any()) } returns 0
+        every { Log.d(any(), any()) } returns 0
+
+        openID4VP = OpenID4VP("test-OpenID4VP")
+
+        openID4VP.authorizationRequest = authorizationRequest
+        setField(
+            openID4VP,
+            "responseUri",
+            responseUrl
+        )
+    }
+
+    @After
+    fun tearDown() {
+        clearAllMocks()
+    }
+
+    @Test
+    fun `should authenticate verifier successfully`() {
+        mockkObject(AuthorizationRequest.Companion)
+
+        val trustedVerifiers = trustedVerifiers
+        val walletMetadata = walletMetadata
+
+        every {
+            AuthorizationRequest.validateAndCreateAuthorizationRequest(
+                any(), any(), any(), any(), any()
+            )
+        } returns authorizationRequest
+
+        val result = openID4VP.authenticateVerifier(
+            "openid-vc://?request=test-request",
+            trustedVerifiers,
+            true,
+            walletMetadata
+        )
+
+        assertEquals(authorizationRequest, result)
+        verify {
+            AuthorizationRequest.validateAndCreateAuthorizationRequest(
+                "openid-vc://?request=test-request", trustedVerifiers, walletMetadata, any(), true
+            )
+        }
+    }
+
+    @Test(expected = Exceptions.InvalidInput::class)
+    fun `should throw exception during verifier authentication`() {
+        mockkObject(AuthorizationRequest.Companion)
+        mockkObject(NetworkManagerClient)
+
+        val testException = Exceptions.InvalidInput("","Invalid authorization request")
+
+        every {
+            AuthorizationRequest.validateAndCreateAuthorizationRequest(
+                any(), any(), any(), any(), any()
+            )
+        } throws testException
+
+        every {
+            NetworkManagerClient.sendHTTPRequest(
+                any(), any(), any()
+            )
+        } returns mapOf("body" to "Error sent")
+
+        openID4VP.authenticateVerifier("openid-vc://?request=invalid", trustedVerifiers)
+    }
+
+    @Test
+    fun `should construct unsigned VP token successfully`() {
+        mockkObject(UUIDGenerator)
+        mockkObject(URDNA2015Canonicalization)
+        mockkStatic(JsonLDObject::class)
+
+        every { UUIDGenerator.generateUUID() } returns "test-uuid-123"
+        every { URDNA2015Canonicalization.canonicalize(any()) } returns "{\"valid\":\"json\"}"
+        every { JsonLDObject.fromJson(any<String>()) } returns JsonLDObject()
+
+        mockkConstructor(UnsignedLdpVPTokenBuilder::class)
+        every { anyConstructed<UnsignedLdpVPTokenBuilder>().build() } returns mapOf(
+            "unsignedVPToken" to unsignedLdpVPToken,
+            "vpTokenSigningPayload" to vpTokenSigningPayload
+        )
+
+        mockkConstructor(UnsignedMdocVPTokenBuilder::class)
+        every { anyConstructed<UnsignedMdocVPTokenBuilder>().build() } returns mapOf(
+            "unsignedVPToken" to unsignedMdocVPToken,
+            "vpTokenSigningPayload" to listOf(mdocCredential)
+        )
+
+        val actualUnsignedVPTokens = openID4VP.constructUnsignedVPToken(
+            selectedLdpCredentialsList + selectedMdocCredentialsList,
+            holderId,
+            signatureSuite
+        )
+
+        val expectedUnsignedVPTokens = unsignedVPTokens
+        assertEquals(expectedUnsignedVPTokens[FormatType.LDP_VC]!!["unsignedVPToken"], actualUnsignedVPTokens[FormatType.LDP_VC])
+        assertEquals(expectedUnsignedVPTokens[FormatType.MSO_MDOC]!!["unsignedVPToken"], actualUnsignedVPTokens[FormatType.MSO_MDOC])
+    }
+
+    @Test(expected = Exceptions.InvalidData::class)
+    fun `should throw exception during VP token construction with invalid data`() {
+        val mockHandler = mockk<AuthorizationResponseHandler>()
+        val testException = Exceptions.InvalidData("Invalid credential format")
+
+        every {
+            mockHandler.constructUnsignedVPToken(any(), any(), any(), any(), any())
+        } throws testException
+
+        setField(openID4VP, "authorizationResponseHandler", mockHandler)
+
+        every {
+            NetworkManagerClient.sendHTTPRequest(any(), any(), any())
+        } returns mapOf("body" to "Error sent")
+
+        openID4VP.constructUnsignedVPToken(selectedLdpCredentialsList, holderId, signatureSuite)
+    }
+
+    @Test
+    fun `should send error to verifier successfully`() {
+        every {
+            NetworkManagerClient.sendHTTPRequest(
+                "https://mock-verifier.com/response-uri",
+                HttpMethod.POST,
+                any()
+            )
+        } returns mapOf("body" to "VP share success")
+
+        openID4VP.sendErrorToVerifier(Exceptions.InvalidData("Unsupported response_mode"))
+
+        verify {
+            NetworkManagerClient.sendHTTPRequest(
+                url = "https://mock-verifier.com/response-uri",
+                method = HttpMethod.POST,
+                bodyParams = mapOf("error" to "Unsupported response_mode")
+            )
+        }
+    }
+
+    @Test
+    fun `should handle exception during sending error to verifier`() {
+        mockkObject(Logger)
+
+        every {
+            NetworkManagerClient.sendHTTPRequest(
+                any(),
+                any(),
+                any()
+            )
+        } throws Exception("Network error")
+
+        every { Logger.error(any(), any()) } just runs
+
+        // Should not throw exception
+        openID4VP.sendErrorToVerifier(Exceptions.InvalidData("Test error"))
+
+        verify {
+            Logger.error(
+                any(),
+                match<Exception> { it.message!!.contains("Unexpected error occurred") }
+            )
+        }
+    }
+
+    @Test
+    fun `should handle deprecated constructVerifiablePresentationToken method`() {
+        val mockHandler = mockk<AuthorizationResponseHandler>()
+
+        every {
+            mockHandler.constructUnsignedVPTokenV1(any(), any(), any())
+        } returns "Deprecated VP Token"
+
+        setField(openID4VP, "authorizationResponseHandler", mockHandler)
+
+        val verifiableCredentials = mapOf("id1" to listOf("vc1", "vc2"))
+
+        val result = openID4VP.constructVerifiablePresentationToken(verifiableCredentials)
+
+        assertEquals("Deprecated VP Token", result)
+    }
+
+    @Test
+    fun `should handle deprecated shareVerifiablePresentation method`() {
+        val mockHandler = mockk<AuthorizationResponseHandler>()
+        val vpResponseMetadata = mockk<VPResponseMetadata>()
+
+        every {
+            mockHandler.shareVPV1(any(), any(), any())
+        } returns "Deprecated VP Sharing Result"
+
+        setField(openID4VP, "authorizationResponseHandler", mockHandler)
+
+        val result = openID4VP.shareVerifiablePresentation(vpResponseMetadata)
+
+        assertEquals("Deprecated VP Sharing Result", result)
+    }
+
+    @Test(expected = Exceptions.InvalidData::class)
+    fun `should handle exception in deprecated constructVerifiablePresentationToken method`() {
+        val mockHandler = mockk<AuthorizationResponseHandler>()
+        val exception = Exceptions.InvalidData("Invalid VC format")
+
+        every {
+            mockHandler.constructUnsignedVPTokenV1(any(), any(), any())
+        } throws exception
+
+        every {
+            NetworkManagerClient.sendHTTPRequest(any(), any(), any())
+        } returns mapOf("body" to "Error sent")
+
+        setField(openID4VP, "authorizationResponseHandler", mockHandler)
+
+        openID4VP.constructVerifiablePresentationToken(mapOf("id1" to listOf("vc1")))
+    }
+
+    @Test
+    fun `should handle empty credential list`() {
+        mockkObject(UUIDGenerator)
+        every { UUIDGenerator.generateUUID() } returns "test-uuid-123"
+
+        val mockHandler = mockk<AuthorizationResponseHandler>()
+        every {
+            mockHandler.constructUnsignedVPToken(any(), any(), any(), any(), any())
+        } returns emptyMap()
+
+        setField(openID4VP, "authorizationResponseHandler", mockHandler)
+
+        val result = openID4VP.constructUnsignedVPToken(emptyMap(), holderId, signatureSuite)
+
+        assertTrue(result.isEmpty())
+    }
+}
--- kotlin/openID4VP/src/test/java/io/mosip/openID4VP/authorizationResponse/unsignedVPToken/types/ldp/UnsignedLdpVPTokenBuilderTest.kt
+++ kotlin/openID4VP/src/test/java/io/mosip/openID4VP/authorizationResponse/unsignedVPToken/types/ldp/UnsignedLdpVPTokenBuilderTest.kt
@@ -1,4 +1,4 @@
-package io.mosip.openID4VP.authorizationResponse.unsignedVPToken.types.ldp
+//package io.mosip.openID4VP.authorizationResponse.unsignedVPToken.types.ldp
 //
 //import android.util.Log
 //import io.mockk.clearAllMocks
@@ -71,3 +71,184 @@ package io.mosip.openID4VP.authorizationResponse.unsignedVPToken.types.ldp
 //        )
 //    }
 //}
+
+package io.mosip.openID4VP.authorizationResponse.unsignedVPToken.types.ldp
+
+import io.mockk.every
+import io.mockk.mockkObject
+import io.mockk.unmockkAll
+import io.mosip.openID4VP.authorizationResponse.vpToken.types.ldp.LdpVPToken
+import io.mosip.openID4VP.common.DateUtil
+import io.mosip.openID4VP.common.URDNA2015Canonicalization
+import io.mosip.openID4VP.constants.SignatureAlgorithm
+import io.mosip.openID4VP.testData.ldpCredential1
+import io.mosip.openID4VP.testData.ldpCredential2
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class UnsignedLdpVPTokenBuilderTest {
+
+    private val verifiableCredentials = listOf(ldpCredential1, ldpCredential2)
+    private val id = "649d581c-f291-4969-9cd5-2c27385a348f"
+    private val holder = "did:example:123456789"
+    private val challenge = "test-challenge"
+    private val domain = "test-domain.com"
+    private val mockDateTime = "2023-01-01T12:00:00Z"
+    private val mockCanonicalizedData = "canonicalized-data"
+
+    @Before
+    fun setup() {
+        mockkObject(DateUtil)
+        every { DateUtil.formattedCurrentDateTime() } returns mockDateTime
+
+        mockkObject(URDNA2015Canonicalization)
+        every { URDNA2015Canonicalization.canonicalize(any()) } returns mockCanonicalizedData
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun `test build with Ed25519Signature2020`() {
+        // Arrange
+        val builder = UnsignedLdpVPTokenBuilder(
+            verifiableCredential = verifiableCredentials,
+            id = id,
+            holder = holder,
+            challenge = challenge,
+            domain = domain,
+            signatureSuite = SignatureAlgorithm.Ed25519Signature2020.value
+        )
+
+        // Act
+        val result = builder.build()
+
+        // Assert
+        Assert.assertNotNull(result)
+        Assert.assertTrue(result.containsKey("vpTokenSigningPayload"))
+        Assert.assertTrue(result.containsKey("unsignedVPToken"))
+
+        val payload = result["vpTokenSigningPayload"] as LdpVPToken
+        Assert.assertEquals(2, payload.context.size)
+        Assert.assertTrue(payload.context.contains("https://www.w3.org/2018/credentials/v1"))
+        Assert.assertTrue(payload.context.contains("https://w3id.org/security/suites/ed25519-2020/v1"))
+        Assert.assertEquals(listOf("VerifiablePresentation"), payload.type)
+        Assert.assertEquals(verifiableCredentials, payload.verifiableCredential)
+        Assert.assertEquals(id, payload.id)
+        Assert.assertEquals(holder, payload.holder)
+
+        val proof = payload.proof
+        Assert.assertNotNull(proof)
+        Assert.assertEquals(SignatureAlgorithm.Ed25519Signature2020.value, proof?.type)
+        Assert.assertEquals(mockDateTime, proof?.created)
+        Assert.assertEquals(holder, proof?.verificationMethod)
+        Assert.assertEquals(domain, proof?.domain)
+        Assert.assertEquals(challenge, proof?.challenge)
+
+        val unsignedToken = result["unsignedVPToken"] as UnsignedLdpVPToken
+        Assert.assertEquals(mockCanonicalizedData, unsignedToken.dataToSign)
+    }
+
+    @Test
+    fun `test build with JsonWebSignature2020`() {
+        // Arrange
+        val builder = UnsignedLdpVPTokenBuilder(
+            verifiableCredential = verifiableCredentials,
+            id = id,
+            holder = holder,
+            challenge = challenge,
+            domain = domain,
+            signatureSuite = SignatureAlgorithm.JsonWebSignature2020.value
+        )
+
+        // Act
+        val result = builder.build()
+
+        // Assert
+        Assert.assertNotNull(result)
+
+        val payload = result["vpTokenSigningPayload"] as LdpVPToken
+        Assert.assertEquals(2, payload.context.size)
+        Assert.assertTrue(payload.context.contains("https://www.w3.org/2018/credentials/v1"))
+        Assert.assertTrue(payload.context.contains("https://w3id.org/security/suites/jws-2020/v1"))
+
+        val proof = payload.proof
+        Assert.assertNotNull(proof)
+        Assert.assertEquals(SignatureAlgorithm.JsonWebSignature2020.value, proof?.type)
+    }
+
+    @Test
+    fun `test build with unknown signature suite`() {
+        // Arrange
+        val unknownSignatureSuite = "UnknownSignatureSuite"
+        val builder = UnsignedLdpVPTokenBuilder(
+            verifiableCredential = verifiableCredentials,
+            id = id,
+            holder = holder,
+            challenge = challenge,
+            domain = domain,
+            signatureSuite = unknownSignatureSuite
+        )
+
+        // Act
+        val result = builder.build()
+
+        // Assert
+        Assert.assertNotNull(result)
+
+        val payload = result["vpTokenSigningPayload"] as LdpVPToken
+        Assert.assertEquals(1, payload.context.size)
+        Assert.assertTrue(payload.context.contains("https://www.w3.org/2018/credentials/v1"))
+
+        val proof = payload.proof
+        Assert.assertNotNull(proof)
+        Assert.assertEquals(unknownSignatureSuite, proof?.type)
+    }
+
+    @Test
+    fun `test build with empty verifiable credential list`() {
+        // Arrange
+        val builder = UnsignedLdpVPTokenBuilder(
+            verifiableCredential = emptyList(),
+            id = id,
+            holder = holder,
+            challenge = challenge,
+            domain = domain,
+            signatureSuite = SignatureAlgorithm.Ed25519Signature2020.value
+        )
+
+        // Act
+        val result = builder.build()
+
+        // Assert
+        Assert.assertNotNull(result)
+
+        val payload = result["vpTokenSigningPayload"] as LdpVPToken
+        Assert.assertTrue(payload.verifiableCredential.isEmpty())
+    }
+
+    @Test
+    fun `test canonicalization error handling`() {
+        // Arrange
+        every { URDNA2015Canonicalization.canonicalize(any()) } throws RuntimeException("Canonicalization failed")
+
+        val builder = UnsignedLdpVPTokenBuilder(
+            verifiableCredential = verifiableCredentials,
+            id = id,
+            holder = holder,
+            challenge = challenge,
+            domain = domain,
+            signatureSuite = SignatureAlgorithm.Ed25519Signature2020.value
+        )
+
+        // Act & Assert
+        val exception = Assert.assertThrows(RuntimeException::class.java) {
+            builder.build()
+        }
+        Assert.assertEquals("Canonicalization failed", exception.message)
+    }
+}
