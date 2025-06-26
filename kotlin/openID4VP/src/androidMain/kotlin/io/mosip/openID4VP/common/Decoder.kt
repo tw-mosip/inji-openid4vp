3438fb7677474ddc8408c2089681644261f15531
BalachandarG <balachandar.g@thoughtworks.com>
2025-06-26 17:48:55 +0530

    [INJIMOB-3317]: Bud fix during VP Verification
    Signed-off-by: BalachandarG <balachandar.g@thoughtworks.com>

--- kotlin/openID4VP/src/androidMain/kotlin/io/mosip/openID4VP/common/Decoder.kt
+++ kotlin/openID4VP/src/androidMain/kotlin/io/mosip/openID4VP/common/Decoder.kt
@@ -1,7 +1,24 @@
 package io.mosip.openID4VP.common
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.util.Base64
+import io.mosip.vercred.vcverifier.utils.BuildConfig.getVersionSDKInt
 
 actual fun decodeBase64Data(content: String): ByteArray {
+    val decodedBase64ByteArray =
+        if( getVersionSDKInt() >= Build.VERSION_CODES.O){
+            javaBase64UrlDecode(content)
+        } else {
+            androidBase64UrlDecode(content)
+        }
+    return decodedBase64ByteArray
+}
+
+@SuppressLint("NewApi")
+private fun javaBase64UrlDecode(content: String): ByteArray =
+    java.util.Base64.getUrlDecoder().decode(content.toByteArray())
+
+private fun androidBase64UrlDecode(content: String): ByteArray {
     return Base64.decode(content, Base64.DEFAULT or Base64.URL_SAFE)
 }
--- kotlin/sampleovpwallet/src/main/java/io/mosip/sampleapp/utils/OpenID4VPManager.kt
+++ kotlin/sampleovpwallet/src/main/java/io/mosip/sampleapp/utils/OpenID4VPManager.kt
@@ -1,6 +1,5 @@
 package io.mosip.sampleapp.utils
 
-
 import android.util.Log
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.nimbusds.jose.jwk.OctetKeyPair
