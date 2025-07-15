5799c72893c8f676ed6568e79e7d6ef63c1b53e1
Alka Prasad <prasadalka1998@gmail.com>
2025-07-15 09:49:04 +0530

    [INJIMOB-3388]: update readme with breaking changes (#101)
    * [INJIMOB-3388]: update readme with breaking changes
    
    Signed-off-by: Alka <prasadalka1998@gmail.com>
    
    * [INJIMOB-3388]: update readme
    
    Signed-off-by: Alka <prasadalka1998@gmail.com>
    
    ---------
    
    Signed-off-by: Alka <prasadalka1998@gmail.com>

--- kotlin/openID4VP/src/commonMain/kotlin/io/mosip/openID4VP/constants/FormatType.kt
+++ kotlin/openID4VP/src/commonMain/kotlin/io/mosip/openID4VP/constants/FormatType.kt
@@ -2,7 +2,13 @@ package io.mosip.openID4VP.constants
 
 enum class FormatType(val value: String)  {
   LDP_VC("ldp_vc"),
-  MSO_MDOC("mso_mdoc"),
+  MSO_MDOC("mso_mdoc");
+
+  companion object {
+    fun fromValue(value: String): FormatType? {
+      return FormatType.entries.find { it.value == value }
+    }
+  }
 }
 
 enum class VPFormatType(val value: String)  {
